661211b7db1d5a91e729cd7a176a11276292c517
MEMEdev <jakubszafarski001@gmail.com>
2024-03-17 21:11:53 +0000

    design tweaks

--- TelegramTradingBot/TradingTelegramBot.py
+++ TelegramTradingBot/TradingTelegramBot.py
@@ -187,7 +187,7 @@ async def ping_all_subscribers():  # when a token is abot to get pinged generate
                                                     f"Level : *{risk_level}*\n🍬 Minted : *{get_mint_epoch}*\n\n📈 [Token Chart]("
                                                     f"https://dexscreener.com/solana/{token_ca})"
                                                     f"\n📱 [Telegram]("
-                                                    f"http://www.example.com/)\n\n😁 *Funding Wallet :* \n*{fund_wallet}\n*👝 [Deployer]("
+                                                    f"http://www.example.com/)\n\n😁 [Funding Wallet :](https://solscan.io/account/{funding_wallet_info})\n*{fund_wallet}\n*👝 [Deployer]("
                                                     f"https://solscan.io/account/{deployer})\n🗃 Deployer Balances : \n*{deployer_balances}* \n📚"
                                                     f"Dev's Previous"
                                                     f" Projects: {past_tokens_string}",
